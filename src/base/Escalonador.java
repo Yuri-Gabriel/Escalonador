ee134b906e7987f89c44e4030b790a45d0837197
/Yuri <67924730+Yuri-Gabriel@users.noreply.github.com>
2024-09-10 11:39:32 -0300

    Update Escalonador.java

--- src/base/Escalonador.java
+++ src/base/Escalonador.java
@@ -1,55 +1,85 @@
 package base;
 
-public class Escalonador {
-	
-	private Processo[] processos;
-	protected double tempoMedioEspera;
-	protected double tempoMedioSaida;
-	
-	public Escalonador(Processo[] processos) {
-		Processo[] copia = new Processo[processos.length];
-		for(int i = 0; i < processos.length; i++) {
-			copia[i] = processos[i];
-		}
-		this.setProcessos(copia);
-	}
-	
-	
-	public double getTempoMedioSaida() {
-		return this.tempoMedioSaida;
-	}
-	
-	public void setTempoMedioSaida() {
-		double total = 0;
-		double tempoParaFinalizar = 0;
-		for(int i = 0; i < this.getProcessos().length; i++) {
-			for(int j = 0; j <= i; j++) {
-				tempoParaFinalizar += this.getProcessos()[j].getSurto();
-			}
-			total += tempoParaFinalizar;
-			tempoParaFinalizar = 0;
-		}
-		this.tempoMedioSaida = total / this.getProcessos().length;
-	}
-	
-	public double getTempoMedioEspera() {
-		return this.tempoMedioEspera;
-	}
-	
-	public void setTempoMedioEspera() {
-		double total = 0;
-		for(int i = 0; i < this.getProcessos().length; i++) {
-			for(int j = 0; j < i; j++) {
-				total += this.getProcessos()[j].getSurto();
-			}
-		}
-		this.tempoMedioEspera = total / this.getProcessos().length;
-	}
-	
-	public Processo[]getProcessos() {
-		return this.processos;
-	}
-	public void setProcessos(Processo[] processos) {
-		this.processos = processos;
-	}
+public abstract class Escalonador {
+
+  private Processo[] processos;
+  protected double tempoMedioEspera;
+  protected double tempoMedioSaida;
+   private int tempoTotalExecucao;
+
+  public Escalonador(Processo[] processos) {
+    Processo[] copia = new Processo[processos.length];
+    for(Processo p : copia) {
+      p = new Processo();
+    }
+    for(int i = 0; i < processos.length; i++) {
+      copia[i] = new Processo(processos[i].getName(), processos[i].getChegada(), processos[i].getSurto());
+    }
+    this.setProcessos(copia);
+    this.setTempoTotalExecucao();
+  }
+
+  public void start() {
+    Processo atual = new Processo();
+    for(int i = 0; i < this.getProcessos().length; i++) {
+      if(this.getProcessos()[i].getStatus() == StatusProcesso.PRONTO) {
+        atual = this.getProcessos()[i];
+        atual.setStatus(StatusProcesso.EXECUTANDO);
+        while(atual.getSurto() > 0) {
+          atual.setSurto(atual.getSurto() - 1);
+        }
+        atual.setStatus(StatusProcesso.FINALIZADO);
+      }
+    }
+  }
+
+  public double getTempoMedioSaida() {
+    return this.tempoMedioSaida;
+  }
+
+  public void setTempoMedioSaida() {
+    double total = 0;
+    double tempoParaFinalizar = 0;
+    for(int i = 0; i < this.getProcessos().length; i++) {
+      for(int j = 0; j <= i; j++) {
+        tempoParaFinalizar += this.getProcessos()[j].getSurto();
+      }
+      total += tempoParaFinalizar;
+      tempoParaFinalizar = 0;
+    }
+    this.tempoMedioSaida = total / this.getProcessos().length;
+  }
+
+  public double getTempoMedioEspera() {
+    return this.tempoMedioEspera;
+  }
+
+  public void setTempoMedioEspera() {
+    double total = 0;
+    for(int i = 0; i < this.getProcessos().length; i++) {
+      for(int j = 0; j < i; j++) {
+        total += this.getProcessos()[j].getSurto();
+      }
+    }
+    this.tempoMedioEspera = total / this.getProcessos().length;
+  }
+
+  public Processo[] getProcessos() {
+    return this.processos;
+  }
+  public void setProcessos(Processo[] processos) {
+    this.processos = processos;
+  }
+
+  public int getTempoTotalExecucao() {
+    return this.tempoTotalExecucao;
+  }
+
+  private void setTempoTotalExecucao() {
+    int tempo = 0;
+    for(Processo p : this.getProcessos()) {
+      tempo += p.getSurto();
+    }
+    this.tempoTotalExecucao = tempo;
+  }
 }
