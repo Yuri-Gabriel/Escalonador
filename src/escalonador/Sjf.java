2eba9776824a7807b4cb6b4b88387e16885e31b1
/Yuri <67924730+Yuri-Gabriel@users.noreply.github.com>
2024-09-10 11:41:30 -0300

    Update Sjf.java

--- src/escalonador/Sjf.java
+++ src/escalonador/Sjf.java
@@ -1,33 +1,31 @@
+
 package escalonador;
 
 import base.Escalonador;
 import base.Processo;
+import base.StatusProcesso;
 
-public class Sjf extends Escalonador{
-	
-	public Sjf(Processo[] processos) {
-		super(processos);
-		this.ordenarProcessos();
-		this.setTempoMedioSaida();
-		this.setTempoMedioEspera();
-		System.out.println("SJF");
-		System.out.println(this.getTempoMedioEspera());
-		System.out.println(this.getTempoMedioSaida());
-	}
-	
-	private void ordenarProcessos() {
-		Processo backup = null;
-		int i = 0;
-		while(i < this.getProcessos().length - 1) {
-			if(this.getProcessos()[i].getSurto() > this.getProcessos()[i + 1].getSurto()) {
-				backup = this.getProcessos()[i];
-				this.getProcessos()[i] = this.getProcessos()[i + 1];
-				this.getProcessos()[i + 1] = backup;
-				
-				i = 0;
-			} else {
-				i++;
-			}
-		}
-	}
+public class Sjf extends Escalonador {
+
+  public Sjf(Processo[] processos) {
+    super(processos);
+    this.ordenarProcessos();
+    this.setTempoMedioSaida();
+    this.setTempoMedioEspera();
+  }
+
+  private void ordenarProcessos() {
+    Processo backup = null;
+    int i = 0;
+    while (i < this.getProcessos().length - 1) {
+      if (this.getProcessos()[i].getSurto() > this.getProcessos()[i + 1].getSurto()) {
+        backup = this.getProcessos()[i];
+        this.getProcessos()[i] = this.getProcessos()[i + 1];
+        this.getProcessos()[i + 1] = backup;
+        i = 0;
+      } else {
+        i++;
+      }
+    }
+  }
 }
