2b42e6e392d4108b172176e35e7ceb417e5ac1dc
/Yuri <67924730+Yuri-Gabriel@users.noreply.github.com>
2024-09-10 11:40:00 -0300

    Update Processo.java

--- src/base/Processo.java
+++ src/base/Processo.java
@@ -1,43 +1,50 @@
 package base;
 
 public class Processo {
-	
-	private String name;
-	private StatusProcesso status;
-	private int chegada;
-	private int surto;
-	
-	public Processo(String nome, int chegada, int surto) {
-		this.setName(nome);
-		this.setStatus(StatusProcesso.INICIANDO);
-		this.setChegada(chegada);
-		this.setSurto(surto);
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public StatusProcesso getStatus() {
-		return this.status;
-	}
-	public void setStatus(StatusProcesso iniciando) {
-		this.status = iniciando;
-	}
-	public int getSurto() {
-		return this.surto;
-	}
-	public void setSurto(int tempoDeSurto) {
-		this.surto = tempoDeSurto;
-	}
 
-	public int getChegada() {
-		return chegada;
-	}
+  private String name;
+  private StatusProcesso status;
+  private int chegada;
+  private int surto;
 
-	public void setChegada(int chegada) {
-		this.chegada = chegada;
-	}
+  public Processo() {
+    this.setName("");
+    this.setStatus(StatusProcesso.INICIANDO);
+    this.setChegada(0);
+    this.setSurto(0);
+  }
+  
+  public Processo(String nome, int chegada, int surto) {
+    this.setName(nome);
+    this.setStatus(StatusProcesso.INICIANDO);
+    this.setChegada(chegada);
+    this.setSurto(surto);
+  }
+
+  public String getName() {
+    return this.name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public StatusProcesso getStatus() {
+    return this.status;
+  }
+  public void setStatus(StatusProcesso iniciando) {
+    this.status = iniciando;
+  }
+  public int getSurto() {
+    return this.surto;
+  }
+  public void setSurto(int tempoDeSurto) {
+    this.surto = tempoDeSurto;
+  }
+
+  public int getChegada() {
+    return chegada;
+  }
+
+  public void setChegada(int chegada) {
+    this.chegada = chegada;
+  }
 }
