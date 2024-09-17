f356c3901939f05947ca864d80ad9d5b41fc2897
/Yuri <67924730+Yuri-Gabriel@users.noreply.github.com>
2024-09-17 12:53:50 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,22 @@
-package main;
-
 import base.Processo;
+
 import escalonador.Fifo;
 import escalonador.Sjf;
+import escalonador.Strf;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Processo[] p = {new Processo("p1", 0, 24), new Processo("p2", 0, 3), new Processo("p3", 0, 3)};
-		
-		
-		Sjf s = new Sjf(p);
-		System.out.println("---------------");
-		Fifo f = new Fifo(p);
-	}
+    public static void main(String[] args) {
+        Processo[] p = { new Processo("p1", 0, 7), new Processo("p2", 2, 4), new Processo("p3", 4, 1), new Processo("p4", 5, 4) };
+        Fifo fifo = new Fifo(p);
+        Sjf sjf = new Sjf(p);
+        Strf strf = new Strf(p);
+        
+        strf.start();
+        
+        
+       
+
+    }
 
 }
